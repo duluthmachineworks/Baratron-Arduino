801eca64a17b4d8c26ebe2ef07c7b636a4d2bdc7
cleeUC <68129277+cleeUC@users.noreply.github.com>
2023-08-29 19:24:25 -0400

    Added files

--- src/Baratron.h
+++ src/Baratron.h
@@ -1,5 +1,20 @@
 // Baratron.h
 
+/*This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 /***************************************************************************
 * Create a class for Baratron objects, using
 * user selected values.
@@ -7,15 +22,13 @@
 * In general, this works by reading the analog input pin assigned to the gauge
 * and then converting the analog input to voltage (0-5v). This is then compared
 * to the voltage range of the gauge (provided by the user as the maximum PSIa
-* rating). With Baratron gauges, as well as most capacitance manometers, 
+* rating). With Baratron gauges, as well as most capacitance manometers,
 * 0v = 0psia, 5v=max psia. Yes, this means that gauges must be zeroed to a hard
 * vacuum.
  ****************************************************************************/
-
+#include "Arduino.h"
 #ifndef Baratron_h
 #define Baratron_h
-
-#include <Arduino.h>
 
 class Baratron {
 private:
